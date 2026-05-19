574ecd4ca9bcfe88eebce9b33a87f460ab4e88e5
GOUTHAMBB <gouthambb999@gmail.com>
2026-05-19 19:34:28 +0530

    Stop tracking ignored file

--- Practise.py
+++
@@ -1,9 +0,0 @@
-def character_hashing(s1,l1):
-    result_list=[0]*123
-    dict1=dict()
-    for i in range(0,len(s1)):
-        result_list[ord(s1[i])]+=1
-    for i in range(0,len(l1)):
-        dict1[l1[i]]=result_list[ord(l1[i])]
-    return dict1
-print(character_hashing("hello world",["h","e","l","o","w","r","d"]))
