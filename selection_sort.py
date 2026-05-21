f5881bdd11a8c20cc75e952d0cca3918e4ab1df1
GOUTHAMBB <gouthambb999@gmail.com>
2026-05-22 00:12:00 +0530

    Updated selection sort code

---
+++ selection_sort.py
@@ -0,0 +1,8 @@
+def selection_sort(arr):
+    for i in range(0,len(arr)-1):
+        for j in range(i,len(arr)):
+            if arr[i]>arr[j]:
+                arr[i],arr[j]=arr[j],arr[i]
+    return arr
+print(selection_sort([5,4,3,2,1]))
+# time complexity is O(n^2) as we are using two nested loops to sort the array and space complexity is O(1) as we are sorting the array in place without using any extra space. 
